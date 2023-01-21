d083ca39ae7b0f9cc1257e8826307a3d1b4ff571
kristoferfannar <kristoferfannarb@gmail.com>
2023-01-21 13:19:37 +0000

    8:calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0) return 0;
+  
+  return arr.reduce((total, num) => {
+    return total + num; 
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => {
+    return total * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  while (num > 1) {
+    factorial *= num--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
