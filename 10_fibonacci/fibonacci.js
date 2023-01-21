f0ff73cb33b4c5535438defa6b964c8fd3a00079
kristoferfannar <kristoferfannarb@gmail.com>
2023-01-21 17:38:31 +0000

    10:fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(times) {
+    times = +times;
+    if (times < 0) return "OOPS";
+    if (times == 0 || times == 1) return times;
 
+    return fibonacci(times - 1) + fibonacci(times - 2);
 };
 
 // Do not edit below this line
