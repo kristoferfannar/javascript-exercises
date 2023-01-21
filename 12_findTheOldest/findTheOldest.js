4e238e1e8d8f28657292447efdd6f6c3779a208e
kristoferfannar <kristoferfannarb@gmail.com>
2023-01-21 17:51:56 +0000

    12: find the oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestSort = people.sort((a, b) => {
+        let ageA = findAge(a);
+        let ageB = findAge(b);
+        
+        if (ageA >= ageB) return -1;
+        return 1
+    })
+    
+    return oldestSort[0];
 };
+
+function findAge(person) {
+    return person.yearOfDeath === undefined ? (new Date()).getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
