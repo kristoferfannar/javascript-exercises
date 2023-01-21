224329a174c2db94a4a9a089b7d0563ee44debe6
kristoferfannar <kristoferfannarb@gmail.com>
2023-01-21 17:33:27 +0000

    9:palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let normalWord = "";
+    let reverseWord = "";
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        let asciiNum = word.charCodeAt(i);
+        if (65 <= asciiNum && asciiNum <= 90 || 97 <= asciiNum && asciiNum <= 121) {
+            reverseWord += word[i];
+            normalWord = word[i] + normalWord;
+        }
+    }
+    return reverseWord.toLowerCase() === normalWord.toLowerCase();
 };
 
 // Do not edit below this line
